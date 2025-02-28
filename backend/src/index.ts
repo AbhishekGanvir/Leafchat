511a0eed5338b261c5de9a3308ab436dc3e0aa17
Abhishek Ganvir <abhiganvir11@gmaile.com>
2025-02-28 16:52:14 +0530

    initial commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import dotenv from "dotenv";
 import { app, server } from "./socket/socket.js";
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const __dirname = path.resolve();
 
 
